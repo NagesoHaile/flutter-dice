5ae7dec6f0022cb20d0e9401ed6430240cbe2fb3
Nageso Haile <nagesoh21@gmail.com>
2023-07-27 14:35:10 +0300

    bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,17 @@
-import 'dart:math';
+//ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text(
-            "Dicee",
-          ),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: MaterialButton(
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
-                child: Image.asset(
-                  'images/dice$leftDiceNumber.png',
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: MaterialButton(
-                onPressed: () {
-                  setState(() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                    rightDiceNumber = Random().nextInt(6) + 1;
-                  });
-                },
-                child: Image.asset(
-                  'images/dice$rightDiceNumber.png',
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
